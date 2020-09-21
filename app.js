c9d253952e20d2ffe59ed3f0d116020ff37d1bcb
davut kok <davut84@yhoo.com>
2020-09-21 21:05:53 +0300

    reloaded

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const rate = document.getElementById("rate");
-const number = document.getElementById("number");
+const amount = document.getElementById("amount");
 const select1 = document.getElementById("select1");
 const select2 = document.getElementById("select2");
-const result = document.getElementById("result");
+const totalAmount = document.getElementById("totalAmount");
 const base1 = document.getElementById("base1");
 const base2 = document.getElementById("base2");
 const end1 = document.getElementById("end1");
 const end2 = document.getElementById("end2");
 
-let select1Value;
-let select2Value;
-
-number.addEventListener("input", handleChange);
+amount.addEventListener("input", handleChange);
 select1.addEventListener("change", handleSelect1);
 select2.addEventListener("change", handleSelect2);
 
+let select1Value;
+let select2Value;
+
+//Handle First Select Input And Set the Rate Value
+function handleSelect1(e) {
+  select1Value = e.target.value;
+  select2Value = select2.value;
+  let base = e.target.value;
+  base1.innerText = base;
+  base2.innerText = base;
+  end1.innerText = select2Value;
+  end2.innerText = select2Value;
+  totalAmount.innerText = "0";
+  amount.value = "";
+  if (select1Value === "EUR" && select2Value === "EUR") {
+    rate.innerHTML = "1";
+  } else {
+    getData(base).then(
+      (r) => (rate.innerHTML = r.rates[select2Value].toFixed(2))
+    );
+  }
+}
+
+//Handle Second Select Input And Set the Rate Value
 function handleSelect2(e) {
   select1Value = select1.value;
   select2Value = e.target.value;
@@ -23,9 +44,9 @@ function handleSelect2(e) {
   base2.innerText = base;
   end1.innerText = select2Value;
   end2.innerText = select2Value;
-  result.innerText = "0";
-  number.value = "";
-  if (select2Value === "EUR" && select2Value === "EUR") {
+  totalAmount.innerText = "0";
+  amount.value = "";
+  if (select1Value === "EUR" && select2Value === "EUR") {
     rate.innerHTML = "1";
   } else {
     getData(base).then(
@@ -34,35 +55,15 @@ function handleSelect2(e) {
   }
 }
 
-function handleSelect1(e) {
-  select1Value = e.target.value;
-  select2Value = select2.value;
-  console.log(select1Value, select2Value);
-  let base = e.target.value;
-  console.log(base);
-  base1.innerText = base;
-  base2.innerText = base;
-  end1.innerText = select2Value;
-  end2.innerText = select2Value;
-  result.innerText = "0";
-  number.value = "";
-  if (base === "EUR" && select2Value === "EUR") {
-    rate.innerHTML = "1";
-  } else {
-    getData(base).then(
-      (r) => (rate.innerHTML = r.rates[select2Value].toFixed(2))
-    );
-  }
-}
-
+//Handle the Total Amount Input And Calculate the Total Exchange Amount
 function handleChange(e) {
-  let resultVal;
-  const val = +e.target.value;
-  resultVal = val * +rate.innerText;
-  result.innerText = resultVal.toFixed(2);
+  const amount = +e.target.value;
+  let totalAmount = amount * +rate.innerText;
+  totalAmount.innerText = totalAmount.toFixed(2);
 }
 
-async function getData(base) {
+//Fetch Data From exchangeratesapi
+async function getData(base = "USD") {
   const response = await fetch(
     `https://api.exchangeratesapi.io/latest?base=${base}`
   );
@@ -70,8 +71,9 @@ async function getData(base) {
   return data;
 }
 
+// When Page First open setting default parameters
 function init() {
-  getData("USD").then((r) => (rate.innerHTML = r.rates.TRY.toFixed(2)));
+  getData().then((r) => (rate.innerHTML = r.rates.TRY.toFixed(2)));
   base1.innerText = "USD";
   base2.innerText = "USD";
   end1.innerText = "TRY";
